69fb890e7c3a0e43e9eb71b19ed80d80369a8c21
Michael Lor and Polina Nenchev <michael.vang.1985@gmail.com>
2016-01-13 11:27:49 -0800

    add if statements and variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
-$(function() {
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+    var gender = $("select#gender").val();
+    var food = $("select#food").val();
+    var drink = $("select#drink").val();
+
+    if (gender === 'male' && food === 'vegetarian' && drink === 'wine') {
+      $("#person1").show()}
+
+    if (gender === 'female' && food === 'mexican' && drink === 'liquor') {
+      $("#person2").show()}
+
+    if (gender === 'male' && food === 'mexican' && drink === 'wine') {
+      $("#person3").show()}
+
+    if (gender === 'female' && food === 'mediterranean' && drink === 'beer') {
+      $("#person4").show()}
+
+    if ("#survey")
+      alert("Sorry you are not compatible with anyone!");
+
+    //
+    // if (gender === 'female' && food === 'mexican' || 'mediterranean' && drink !== 'wine') {
+    // } $("#person2").show()
 
 
+    // $("#gender").empty().append("quote");
+    $("#quote").show();
+    return false;
+    //
+    // if (gender === 'female' && food === 'mexican' || 'mediterranean' && drink !== 'wine') {
+    // } else {
+    //
+    // }
 
-
-
+    event.preventDefault();
+  });
 });
